115f28cc4e54bdd59f0e4ea29d346fdb5140dbd2
sugam206 <sugamsubedi23@gmail.com>
2025-08-29 21:33:25 +0545

    haha

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <>
-        <p className='text-emerald-400 '> behenchod k ho  y yesto</p></>
-    </div>
+    <div>app</div>
   )
 }
 
